ba651ff84bd759c6f7dc71dd83d0bd7957a1dfd6
Peter Acs <peter.acs@stanford.edu>
2018-10-09 12:52:26 -0700

    analysis page modifications #19 - display labels for select modules instead of title field - display labels for parameter keys instead of key field - display description of parameter under each field - add possibility to display a module info box

--- grails_webapp/grails-app/domain/flowgate/Module.groovy
+++ grails_webapp/grails-app/domain/flowgate/Module.groovy
@@ -3,11 +3,19 @@ package flowgate
 class Module {
 
     static hasMany=[moduleParams: ModuleParam]
+    String label
     String title
     String name
+    String descript
     AnalysisServer server
 
     static constraints = {
         moduleParams nullable: true
+        descript nullable: true
+        label nullable: true
+    }
+
+    static mapping = {
+        descript sqlType: 'varchar(4096)'
     }
 }
